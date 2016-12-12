4f2592229a3d0ac62ae78ab8501e01fe0cc7f8b8
LucasFLima <lucas.f.lima@gmail.com>
2016-12-12 12:07:32 -0200

    alteração local

--- projeto1/app/aluno/components/aluno.component.ts
+++ projeto1/app/aluno/components/aluno.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';  
 import { Aluno } from '../class/aluno';
 import { AlunoService } from '../service/aluno.service'
 
